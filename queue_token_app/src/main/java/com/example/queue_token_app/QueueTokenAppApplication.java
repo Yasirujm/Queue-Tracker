ecec5f3fc1eaecd7bad7571225b77f6d9c31b9d8
Lahiru Fernando <lahirufernando802@gmail.com>
2026-01-23 14:13:32 +0530

    initial backend project setup

---
+++ queue_token_app/src/main/java/com/example/queue_token_app/QueueTokenAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.queue_token_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueTokenAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueTokenAppApplication.class, args);
+	}
+
+}
